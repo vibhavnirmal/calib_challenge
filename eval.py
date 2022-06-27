813d7b36f3b1fba4775cf0652fdd7a83e46cffe3
vin <vibhavnirmal@outlook.com>
2022-06-26 23:56:08 -0500

    added eval script

--- eval.py
+++ eval.py
@@ -1,27 +1,48 @@
+import warnings
+
 import numpy as np
-import sys
 
+from main import FeatureDetector, Processing, SelectVideo
 
-if len(sys.argv) > 1:
-  TEST_DIR = sys.argv[1]
-else:
-  raise RuntimeError('No test directory provided')
+F = 910
+height = 880
+width = 1200
+
+K = np.array(([F, 0, width//2], [0, F, height//2], [0, 0, 1]))
+fe = FeatureDetector(K)
+check = Processing(fe)
+
+cap = SelectVideo().select()
+
+myData = []
+isFirstFrame = True
+while cap.isOpened():
+    warnings.simplefilter('ignore', np.RankWarning)
+    warnings.simplefilter("ignore", category=RuntimeWarning)
+    ret, currentFrame = cap.read()
+    if ret == True:
+        currentFrame = currentFrame[0:640, :]
+        img2, yaw, pitch = check.process_frame(currentFrame)
+        myData.append([np.float64(yaw.tolist()[0]),
+                      np.float64(pitch.tolist()[0])])
+    else:
+        break
+
 GT_DIR = 'labeled/'
+gt = np.loadtxt(GT_DIR + str(4) + '.txt')
+
+myData = np.array(myData)
 
 def get_mse(gt, test):
-  test = np.nan_to_num(test)
-  return np.mean(np.nanmean((gt - test)**2, axis=0))
+    test = np.nan_to_num(test)
+    return np.mean(np.nanmean((gt - test)**2, axis=0))
 
 
 zero_mses = []
 mses = []
-
-for i in range(0,5):
-  gt = np.loadtxt(GT_DIR + str(i) + '.txt')
-  zero_mses.append(get_mse(gt, np.zeros_like(gt)))
-
-  test = np.loadtxt(TEST_DIR + str(i) + '.txt')
-  mses.append(get_mse(gt, test))
+zero_mses.append(get_mse(gt, np.zeros_like(gt)))
+mses.append(get_mse(gt, myData))
 
 percent_err_vs_all_zeros = 100*np.mean(mses)/np.mean(zero_mses)
+
 print(f'YOUR ERROR SCORE IS {percent_err_vs_all_zeros:.2f}% (lower is better)')
--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class SelectVideo(object):
         # cap = cv2.VideoCapture('labeled/0.hevc')  #NIGHT HIGHWAY
         # cap = cv2.VideoCapture('labeled/1.hevc')  #NIGHT HIGHWAY
         # cap = cv2.VideoCapture('labeled/2.hevc')  #Day + Blur
-        cap = cv2.VideoCapture('labeled/3.hevc')  # NIGHT CITY
-        # cap = cv2.VideoCapture('labeled/4.hevc')  #Day
+        # cap = cv2.VideoCapture('labeled/3.hevc')  # NIGHT CITY
+        cap = cv2.VideoCapture('labeled/4.hevc')  #Day
         # cap = cv2.VideoCapture('unlabeled/5.hevc')#Day HIGHWAY
         # cap = cv2.VideoCapture('unlabeled/6.hevc')#DAY RAIN
         # cap = cv2.VideoCapture('unlabeled/7.hevc')#NIGHT Suburb
